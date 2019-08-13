ac06a12a134665af232f25c763a36c07f0d6dcda
ksyunnnn <is.syunsukekobashi@gmail.com>
2019-08-13 22:11:55 +0900

    prod

--- src/index.js
+++ src/index.js
@@ -2,34 +2,56 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import "./palette.css";
 
-import Palette, { usePalette } from "./palette";
+const colors = {
+  RED: "#F44336",
+  PINK: "#E91E63",
+  PURPLE: "#9C27B0",
+  "DEEP PURPLE": "#673AB7",
+  INDIGO: "#3F51B5",
+  BLUE: "#2196F3",
+  "LIGHT BLUE": "#03A9F4",
+  CYAN: "#00BCD4",
+  TEAL: "#009688",
+  GREEN: "#4CAF50",
+  "LIGHT GREEN": "#8BC34A",
+  LIME: "#CDDC39",
+  YELLOW: "#FFEB3B",
+  AMBER: "#FFC107",
+  ORANGE: "#FF9800",
+  "DEEP ORANGE": "#FF5722",
+  BROWN: "#795548",
+  GREY: "#9E9E9E",
+  "BLUE GREY": "#607D8B"
+};
 
 const usePersistentCanvas = () => {
   const [canvasSize, setCanvasSize] = useState({ width: 1280, height: 670 });
-  const [canvasColor, setCanvasColor] = useState("#2196F3");
   const [dataURL, setDataURL] = useState("");
 
   const [titleValue, setTitleValue] = useState({
     main: {
-      title: "スピード IS ゴッド",
+      title: "OGP IS ゴッド",
       size: 80,
-      baseline: "bottom"
+      baseline: "bottom",
+      display: true
     },
     sub: {
-      title: "爆速の漫画ビューアへの挑戦",
+      title: "爆速のジェネレーターへの挑戦",
       size: 48,
-      baseline: "top"
+      baseline: "top",
+      display: true
     }
   });
+
+  const [pickColor, setPickColor] = useState(colors["TEAL"]);
 
   const { main, sub } = titleValue;
 
   const canvasRef = useRef(null);
 
   const handleDataUrl = canvas => setDataURL(canvas.toDataURL());
-
-  const [pickColor] = usePalette();
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -46,18 +68,26 @@ const usePersistentCanvas = () => {
     ctx.fillStyle = "#fff";
 
     // about title
-    ctx.textBaseline = main.baseline;
-    ctx.font = `500 ${main.size}px 'Noto Sans JP'`;
-    ctx.fillText(main.title, canterX, canterY - 16);
+    if (main.display) {
+      ctx.textBaseline = main.baseline;
+      ctx.font = `500 ${main.size}px 'Noto Sans JP'`;
+
+      const handleY = sub.display ? -16 : 48;
+
+      ctx.fillText(main.title, canterX, canterY + handleY);
+    }
 
     // about subTitle
-    ctx.textBaseline = sub.baseline;
-    ctx.font = `normal ${sub.size}px 'Noto Sans JP'`;
-    ctx.fillText(sub.title, canterX, canterY + 16);
+    if (sub.display) {
+      ctx.textBaseline = sub.baseline;
+      ctx.font = `normal ${sub.size}px 'Noto Sans JP'`;
+
+      const handleY = main.display ? 16 : -24;
+
+      ctx.fillText(sub.title, canterX, canterY + handleY);
+    }
 
     handleDataUrl(canvas);
-    console.log("main", main);
-    console.log("pickColor", pickColor);
   });
 
   return [
@@ -65,11 +95,30 @@ const usePersistentCanvas = () => {
     setTitleValue,
     canvasSize,
     setCanvasSize,
-    canvasColor,
-    setCanvasColor,
     canvasRef,
-    dataURL
+    dataURL,
+    pickColor,
+    setPickColor,
+    colors
   ];
+};
+
+const Palette = props => {
+  const { handlePickColor, colors } = props;
+  return (
+    <div className="wrapper">
+      {colors &&
+        Object.keys(colors).map(k => (
+          <button
+            className="btn"
+            style={{ background: colors[k] }}
+            onClick={() => {
+              handlePickColor(k);
+            }}
+          />
+        ))}
+    </div>
+  );
 };
 
 const App = () => {
@@ -78,10 +127,11 @@ const App = () => {
     setTitleValue,
     canvasSize,
     setCanvasSize,
-    canvasColor,
-    setCanvasColor,
     canvasRef,
-    dataURL
+    dataURL,
+    pickColor,
+    setPickColor,
+    colors
   ] = usePersistentCanvas();
 
   const handleTitleValue = e => {
@@ -90,57 +140,58 @@ const App = () => {
     setTitleValue(newState);
   };
 
-  // const handleCanvasSize = e => {
-  //   const newState = { ...canvasSize };
-  //   newState[e.target.name] = e.target.value;
-  //   setCanvasSize(newState);
-  // };
-
-  const handleCanvasColor = e => {
-    setCanvasColor(e.target.value);
+  const handleDisplayValue = e => {
+    const newState = { main, sub };
+    newState[e.target.name].display = !newState[e.target.name].display;
+    setTitleValue(newState);
   };
+
+  const handlePickColor = k => setPickColor(colors[k]);
 
   return (
     <div className="app">
-      <input
-        value={main.title}
-        type="text"
-        name="main"
-        onChange={handleTitleValue}
-      />
-      <input
-        value={sub.title}
-        type="text"
-        name="sub"
-        onChange={handleTitleValue}
-      />
-      {/* 一旦削除
-      <input
-        value={canvasSize.width}
-        type="number"
-        name="width"
-        onChange={handleCanvasSize}
-      />
-      <input
-        value={canvasSize.height}
-        type="number"
-        name="height"
-        onChange={handleCanvasSize}
-      />
-       */}
-      {/* <input
-        value={canvasColor}
-        type="text"
-        name="canvas-color"
-        onChange={handleCanvasColor}
-      /> */}
-      <Palette />
+      <h1>OGPジェネレーター🍠</h1>
+      <div className="input">
+        <input
+          value={main.title}
+          type="text"
+          name="main"
+          onChange={handleTitleValue}
+          className={`${main.display ? "" : "hidden"}`}
+          disabled={!main.display}
+        />
+        <button onClick={handleDisplayValue} name="main">
+          {main.display ? (
+            <span id="mieru">👀</span>
+          ) : (
+            <span id="miezaru">🐒</span>
+          )}
+        </button>
+      </div>
+      <div className="input">
+        <input
+          value={sub.title}
+          type="text"
+          name="sub"
+          onChange={handleTitleValue}
+          className={`${sub.display ? "" : "hidden"}`}
+          disabled={!sub.display}
+        />
+        <button onClick={handleDisplayValue} name="sub">
+          {sub.display ? (
+            <span id="mieru">👀</span>
+          ) : (
+            <span id="miezaru">🐒</span>
+          )}
+        </button>
+      </div>
+      <Palette colors={colors} handlePickColor={handlePickColor} />
       <canvas
         ref={canvasRef}
         width={canvasSize.width}
         height={canvasSize.height}
       />
-      <a href={dataURL} download="">
+      <a href={dataURL} download="" className="btn">
         download
       </a>
     </div>
